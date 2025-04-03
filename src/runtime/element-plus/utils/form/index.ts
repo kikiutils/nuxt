90706c9cca13b1f36a51721dc04f4e26ed30d279
kiki-kanri <a470666@gmail.com>
2025-04-04 01:53:23 +0800

    refactor!: update `createElFormItemRule` to remove default message and only handle defaults for `required`, `trigger`, and `type`

--- src/runtime/element-plus/utils/form/index.ts
+++ src/runtime/element-plus/utils/form/index.ts
@@ -1,27 +1,11 @@
 import type { FormItemRule } from 'element-plus';
 
-export function createElFormItemRule(message: string = '', options: FormItemRule = {}): FormItemRule {
+export function createElFormItemRule(message: string, options: FormItemRule = {}): FormItemRule {
     const {
-        asyncValidator,
         required = true,
         trigger = 'blur',
         type = 'string',
-        validator,
     } = options;
-    if (asyncValidator) {
-        return {
-            asyncValidator,
-            trigger,
-        };
-    }
-
-    if (validator) {
-        return {
-            trigger,
-            validator,
-        };
-    }
-
     return {
         ...options,
         message,
