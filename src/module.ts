f31be89b0c4eaefcb2b6bf5c89ff98ce6dcb4395
kiki-kanri <a470666@gmail.com>
2025-05-05 11:36:33 +0800

    feat: initialize module `setup`

--- src/module.ts
+++ src/module.ts
@@ -1,14 +1,28 @@
-import { defineNuxtModule } from '@nuxt/kit';
+import {
+    createResolver,
+    defineNuxtModule,
+    useLogger,
+} from '@nuxt/kit';
 
-import type { ModuleOptions } from './types/options';
+import type {
+    ResolvedModuleOptions,
+    UserModuleOptions,
+} from './types/options';
 
-export default defineNuxtModule<ModuleOptions>({
+export default defineNuxtModule<UserModuleOptions>({
     // Default configuration options of the Nuxt module
-    defaults: {},
+    defaults: { enabled: true } satisfies ResolvedModuleOptions,
     meta: {
         compatibility: { nuxt: '>=3.13.0' },
         configKey: 'kikiutilsNuxt',
         name: '@kikiutils/nuxt',
     },
-    setup(options, nuxt) {},
+    setup(_options, nuxt) {
+        const options = _options as ResolvedModuleOptions;
+        const logger = useLogger();
+        if (!options.enabled) return logger.info('@kikiutils/nuxt is disabled.');
+        logger.info('Initializing @kikiutils/nuxt...');
+        const resolver = createResolver(import.meta.url);
+        logger.success('@kikiutils/nuxt initialized successfully.');
+    },
 });
--- src/types/options.ts
+++ src/types/options.ts
@@ -1,2 +1,11 @@
+import type { RequiredDeep } from 'type-fest';
+
+export type ResolvedModuleOptions = RequiredDeep<UserModuleOptions>;
+
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface UserModuleOptions {
+    /**
+     * @default true
+     */
+    enabled?: boolean;
+}
