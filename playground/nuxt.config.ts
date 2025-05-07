5d35d30175190c202938eeeead8dbbeb8dd4af88
kiki-kanri <a470666@gmail.com>
2025-05-07 11:56:15 +0800

    chore: update playground `nuxt.config` to use full module options

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,10 +1,41 @@
 export default defineNuxtConfig({
     compatibilityDate: '2100-01-01',
     kikiutilsNuxt: {
+        autoImportUtils: {
+            '@kikiutils/shared': {
+                clipboard: true,
+                datetime: true,
+                enhancedLocalStorage: true,
+                enum: true,
+                general: true,
+                hash: true,
+                math: true,
+                number: true,
+                random: true,
+                string: true,
+                url: true,
+                vue: true,
+                web: true,
+            },
+        },
+        enabled: true,
         enabledModules: {
             colorMode: true,
             elementPlus: true,
+            robots: true,
             security: true,
+            unoCss: true,
+            unpluginFonts: true,
+            vueUse: true,
+        },
+        enabledPlugins: { preventDragFileDrop: true },
+        enabledStyles: { reboot: true },
+        unoCss: { enabledResets: { tailwind: true } },
+        unpluginFonts: {
+            google: {
+                addPreconnectLink: true,
+                disableDeferAndAutoAddPreloadLink: true,
+            },
         },
     },
     modules: ['../src/module'],
