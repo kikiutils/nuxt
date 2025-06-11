fae7cd0fe17767065574799089cf8077c74a4526
kiki-kanri <a470666@gmail.com>
2025-06-11 14:11:19 +0800

    refactor: change Nuxt base directory from `rootDir` to `srcDir` in `setupDeepScanAutoImports`

--- src/setups/deep-scan-auto-imports.ts
+++ src/setups/deep-scan-auto-imports.ts
@@ -6,9 +6,9 @@ import type { ResolvedModuleOptions } from '../types/options';
 export function setupDeepScanAutoImports(resolvedModuleOptions: ResolvedModuleOptions, nuxt: Nuxt) {
     if (!resolvedModuleOptions.deepScanAutoImportDirs) return;
     if (resolvedModuleOptions.deepScanAutoImportDirs.composables) {
-        addImportsDir(`${nuxt.options.rootDir}/composables/**/*.ts`);
+        addImportsDir(`${nuxt.options.srcDir}/composables/**/*.ts`);
     }
 
-    if (resolvedModuleOptions.deepScanAutoImportDirs.globals) addImportsDir(`${nuxt.options.rootDir}/globals/**/*.ts`);
-    if (resolvedModuleOptions.deepScanAutoImportDirs.utils) addImportsDir(`${nuxt.options.rootDir}/utils/**/*.ts`);
+    if (resolvedModuleOptions.deepScanAutoImportDirs.globals) addImportsDir(`${nuxt.options.srcDir}/globals/**/*.ts`);
+    if (resolvedModuleOptions.deepScanAutoImportDirs.utils) addImportsDir(`${nuxt.options.srcDir}/utils/**/*.ts`);
 }
