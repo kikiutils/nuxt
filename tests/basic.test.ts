8311ac97ef4ca7b5ecdb66ebc662943e2a472ddd
kiki-kanri <a470666@gmail.com>
2025-07-16 10:16:10 +0800

    chore: upgrade dependencies and migrate nuxt to v4

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -12,6 +12,7 @@ import {
 
 describe('ssr', async () => {
     await setup({ rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)) });
+
     it('renders the index page', async () => {
         // Get response to a server-rendered page with `$fetch`.
         const html = await $fetch('/');
