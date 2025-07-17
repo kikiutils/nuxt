3be64cd2f30bb73e22c4f56d7e6d4871e7e3730e
kiki-kanri <a470666@gmail.com>
2025-07-17 16:25:22 +0800

    fix: set `NuxtCustomSchema.unfonts` type

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,5 +1,6 @@
 declare module '../../.nuxt/schema/nuxt.schema.d.ts' {
     export interface NuxtCustomSchema {
         colorMode?: import('@nuxtjs/color-mode').ModuleOptions;
+        unfonts?: Parameters<typeof import('unplugin-fonts/nuxt').default>[0];
     }
 }
