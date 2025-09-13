fc9e7cb865a65e77017d4a1215de8b1d1a889ff8
kiki-kanri <a470666@gmail.com>
2025-09-13 19:34:00 +0800

    chore: update tsdown config
    - enable dts, exports, publint and unused config
    - set format to esm

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
     alias: { '@/': './src' },
     clean: true,
+    dts: true,
     entry: ['./src/index.ts'],
+    exports: true,
+    format: 'esm',
+    publint: true,
     sourcemap: true,
     tsconfig: './tsconfig.build.json',
     unbundle: true,
+    unused: true,
 });
