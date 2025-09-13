0522f798fb661d9909c37ce7f1788167f80eea1f
kiki-kanri <a470666@gmail.com>
2025-09-13 20:02:42 +0800

    test: fix vitest config to correctly load tsconfig and aliases

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,6 +1,11 @@
+import { resolve } from 'node:path';
+
+import { parse } from 'tsconfck';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    esbuild: { tsconfigRaw: (await parse('./tsconfig.tests.json')).tsconfig },
+    resolve: { alias: { '@': resolve(import.meta.dirname, 'src') } },
     test: {
         coverage: {
             include: ['src/**/*'],
