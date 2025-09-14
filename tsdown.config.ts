735a3a950795ea525c692a0332ad7412de4d53b6
kiki-kanri <a470666@gmail.com>
2025-09-14 22:17:22 +0800

    fix: remove tsdown remove-types-js plugin and custom exports configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,3 @@
-import {
-    glob,
-    rm,
-} from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 import { defineConfig } from 'tsdown';
@@ -16,26 +12,8 @@ export default defineConfig({
         '!./src/**/_internals/**',
         '!./src/**/internals/**',
     ],
-    exports: {
-        customExports(exports) {
-            Object.entries(exports).forEach(([key, value]: [string, string]) => {
-                if (value.startsWith('./dist/types')) exports[key] = { types: value.replace(/\.js$/, '.d.ts') };
-            });
-
-            return exports;
-        },
-    },
+    exports: true,
     format: 'esm',
-    plugins: [
-        {
-            name: 'remove-types-js',
-            async writeBundle() {
-                const files = [];
-                for await (const file of glob('./dist/types/**/*.js')) files.push(file);
-                await Promise.all(files.map((file) => rm(file, { force: true })));
-            },
-        },
-    ],
     publint: true,
     sourcemap: true,
     tsconfig: './tsconfig.build.json',
