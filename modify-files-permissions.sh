643c006effde7903edd8c765c7e0712d18436276
kiki-kanri <a470666@gmail.com>
2025-09-14 11:19:02 +0800

    feat: add `update-peer-dependencies-meta.ts`

--- modify-files-permissions.sh
+++ modify-files-permissions.sh
@@ -10,3 +10,4 @@ find . -name node_modules -prune -o \( -type f -exec chmod 600 {} + \)
 find . -name node_modules -prune -o \( -type d -exec chmod 700 {} + \)
 find . -type d -name bin -exec chmod 700 -R {} +
 find . -name node_modules -prune -o \( -name '*.sh' -type f -exec chmod 700 {} + \)
+chmod 700 ./update-peer-dependencies-meta.ts
