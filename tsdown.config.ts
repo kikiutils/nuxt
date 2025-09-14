6922c2f859e954efd04c4363f1af7d53a4f02400
kiki-kanri <a470666@gmail.com>
2025-09-14 21:47:21 +0800

    fix: remove tsdown `remove-types-js` plugin

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,3 @@
-import {
-    glob,
-    rm,
-} from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 import { defineConfig } from 'tsdown';
@@ -18,16 +14,6 @@ export default defineConfig({
     ],
     exports: true,
     format: 'esm',
-    plugins: [
-        {
-            name: 'remove-types-js',
-            async writeBundle() {
-                const files = [];
-                for await (const file of glob('./dist/types/**/*.js')) files.push(file);
-                await Promise.all(files.map((file) => rm(file, { force: true })));
-            },
-        },
-    ],
     publint: true,
     sourcemap: true,
     tsconfig: './tsconfig.build.json',
