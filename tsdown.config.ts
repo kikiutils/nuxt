2affd3134e0b044ac68a3237a74965fd5aa2997f
kiki-kanri <a470666@gmail.com>
2025-09-14 21:19:01 +0800

    build: clean up js files under `dist/types` after tsdown build and update exports config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,3 +1,7 @@
+import {
+    glob,
+    rm,
+} from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 import { defineConfig } from 'tsdown';
@@ -12,8 +16,26 @@ export default defineConfig({
         '!./src/**/_internals/**',
         '!./src/**/internals/**',
     ],
-    exports: true,
+    exports: {
+        customExports(exports) {
+            Object.entries(exports).forEach(([key, value]: [string, string]) => {
+                if (value.startsWith('./dist/types')) exports[key] = { types: value.replace(/\.js$/, '.d.ts') };
+            });
+
+            return exports;
+        },
+    },
     format: 'esm',
+    plugins: [
+        {
+            name: 'remove-types-js',
+            async writeBundle() {
+                const files = [];
+                for await (const file of glob('./dist/types/**/*.js')) files.push(file);
+                await Promise.all(files.map((file) => rm(file, { force: true })));
+            },
+        },
+    ],
     publint: true,
     sourcemap: true,
     tsconfig: './tsconfig.build.json',
