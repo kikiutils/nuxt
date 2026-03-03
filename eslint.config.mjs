77a5f00cffe4133a8277209e5577bb69097c4103
kiki-kanri <a470666@gmail.com>
2026-03-03 13:25:17 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { antfu } from '@antfu/eslint-config';
 import { createBaseConfigs } from '@kikiutils/eslint-config/base';
+import { createVsCodeJsonConfigs } from '@kikiutils/eslint-config/json';
 
 export default antfu(
     {
@@ -7,5 +8,6 @@ export default antfu(
         typescript: true,
     },
     createBaseConfigs(),
+    createVsCodeJsonConfigs(),
     { rules: { 'ts/explicit-function-return-type': 'off' } },
 );
